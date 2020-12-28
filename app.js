60b332022c07ccf859ae92ca27c19ade9dfaef85
arnamen <aew11111d@gmail.com>
2020-12-28 17:01:47 +0200

    added unknown routes handling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParse = require('body-parser');
 
+const HttpError = require('./models/http-error');
 const usersRoutes = require('./routes/users-routes');
 const itemsRoutes = require('./routes/items-routes');
 
@@ -8,6 +9,12 @@ const app = express();
 
 app.use('/api/users', usersRoutes);
 app.use('/api/items', itemsRoutes);
+
+app.use((req, res, next) => {
+    const error = new HttpError('Could not find this route.', 404);
+    return next(error);
+});
+
 app.use((error, req, res, next) => {
     if(res.headerSent) return next(error);
     res.status(error.code || 500).json({message: error.message || 'An unknown error occured'});
