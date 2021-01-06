e6e7494700884c191a5a5bf73581523b2dc1fb5f
arnamen <aew11111d@gmail.com>
2021-01-06 19:09:11 +0200

    Item schema updates

--- models/Item.js
+++ models/Item.js
@@ -3,16 +3,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const itemchema = new Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  categories: {type: [String], required: true},
-  tags: {type: mongoose.Schema.Types.Mixed}, //объект произвольных тегов (название - значение)
-  imagesPath: { type: [String] },
-  inStock: {type: Boolean, required: true},
-  price: {type: Number, required: true},
-  oldPrice: {type: Number, required: false},
-  stars: {type: Number, required: true},
-  labels: {type: [String], required: false},
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    categories: { type: [String], required: true },
+    tags: { type: mongoose.Schema.Types.Mixed }, //объект произвольных тегов (название - значение)
+    imagesPath: { type: [String] },
+    inStock: { type: Boolean, required: true },
+    price: { type: Number, required: true },
+    oldPrice: { type: Number, required: false },
+    stars: { type: Number, default: 0 },
+    labels: { type: [String], required: false },
+    reviews: {
+        type: [{
+            author: { type: mongoose.Schema.Types.ObjectId, required: true },
+            review: { type: String, required: true }
+        }]
+    },
 });
 
-module.exports = mongoose.model('Place', placeSchema);
+module.exports = mongoose.model('Item', itemchema);
